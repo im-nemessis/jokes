b50aa3e474040bfb184598f0943389df0693f7b4
im-nemessis <powerfulltech15@gmail.com>
2020-02-09 12:17:38 +0530

    errror handler is added and made as a function

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -13,13 +13,25 @@ function getJokes() {
     }
   };
 
-  fetch(url, options).then(function(response) {
+  let errroHandler = response => {
+    if (!response.ok) {
+      h1.innerText =
+        "couldn't find the jokes nework error probbaly check your internet connection ";
+    }
+  };
+
+  let fetching = fetch(url, options);
+
+  fetching.then(function(response) {
+    errroHandler();
     response
       .json()
       .then(function(json) {
         h1.innerText = json.joke;
       })
       .catch(function(error) {
+        h1.innerText =
+          "couldn't find the jokes nework error probbaly check your internet connection ";
         console.log(error);
       });
   });
